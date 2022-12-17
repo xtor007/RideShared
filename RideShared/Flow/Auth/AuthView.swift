3359518c52d321d51779446887fe8aea287fbce3
Anatolii Khramchenko <71408259+xtor007@users.noreply.github.com>
2022-12-17 18:14:46 +0200

    Feature: Project Init (#1)
    * Init project
    
    * Add App icon
    
    * Add localizable
    
    * Add swiftgen

---
+++ RideShared/Flow/Auth/AuthView.swift
@@ -0,0 +1,20 @@
+//
+//  AuthView.swift
+//  RideShared
+//
+//  Created by Anatoliy Khramchenko on 17.12.2022.
+//
+
+import SwiftUI
+
+struct AuthView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AuthView_Previews: PreviewProvider {
+    static var previews: some View {
+        AuthView()
+    }
+}
