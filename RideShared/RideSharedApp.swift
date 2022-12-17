9eb9b72610820b72d398aaf20781c2433726ab41
Anatolii Khramchenko <71408259+xtor007@users.noreply.github.com>
2022-12-17 17:08:10 +0200

    Initial Commit

---
+++ RideShared/RideSharedApp.swift
@@ -0,0 +1,17 @@
+//
+//  RideSharedApp.swift
+//  RideShared
+//
+//  Created by Anatoliy Khramchenko on 17.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct RideSharedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
