5820ddd395f634c2b64033fac9e84c2fd0bd3bfe
Anatolii Khramchenko <71408259+xtor007@users.noreply.github.com>
2022-12-21 20:23:31 +0200

    Add NavigationView

--- RideShared/Flow/Profile/ProfileView.swift
+++ RideShared/Flow/Profile/ProfileView.swift
@@ -12,48 +12,50 @@ struct ProfileView: View {
     @ObservedObject var userManager: UserManager
 
     var body: some View {
-        VStack(spacing: Paddings.padding20) {
-            
-            HStack {
+        NavigationView {
+            VStack(spacing: Paddings.padding20) {
                 
-                Image(
-                    uiImage: userManager.user.avatar.map({ data in
-                        return UIImage(data: data)
-                        ?? Asset.Images.defaultAvatar.image
-                    })
-                       ?? Asset.Images.defaultAvatar.image
-                )
-                .resizable()
-                .scaledToFit()
-                .frame(width: 80)
-                .clipShape(Circle())
+                HStack {
+                    
+                    Image(
+                        uiImage: userManager.user.avatar.map({ data in
+                            return UIImage(data: data)
+                            ?? Asset.Images.defaultAvatar.image
+                        })
+                           ?? Asset.Images.defaultAvatar.image
+                    )
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 80)
+                    .clipShape(Circle())
+                    
+                    Spacer()
+                    
+                    VStack(alignment: .leading, spacing: Paddings.padding10) {
+                        Text(userManager.user.name)
+                            .font(.title)
+                            .lineLimit(1...2)
+                        Text("⭐️\(userManager.user.rating.rating)")
+                            .font(.bold(.subheadline)())
+                    }
+                    .foregroundColor(Color(Asset.Colors.textColor.color))
+                    
+                }
+                .padding(.horizontal, Paddings.padding30)
+                .padding(.top, Paddings.padding30)
+                
+                ScreenListView(data: [
+                    ProfileListElement.prioritets,
+                    ProfileListElement.adresses,
+                    ProfileListElement.driver
+                ])
+                .padding(.horizontal, Paddings.padding20)
                 
                 Spacer()
                 
-                VStack(alignment: .leading, spacing: Paddings.padding10) {
-                    Text(userManager.user.name)
-                        .font(.title)
-                        .lineLimit(1...2)
-                    Text("⭐️\(userManager.user.rating.rating)")
-                        .font(.bold(.subheadline)())
-                }
-                .foregroundColor(Color(Asset.Colors.textColor.color))
-                
             }
-            .padding(.horizontal, Paddings.padding30)
-            .padding(.top, Paddings.padding30)
-            
-            ScreenListView(data: [
-                ProfileListElement.prioritets,
-                ProfileListElement.adresses,
-                ProfileListElement.driver
-            ])
-            .padding(.horizontal, Paddings.padding20)
-            
-            Spacer()
-            
+            .background(Color(Asset.Colors.backgroundColor.color).ignoresSafeArea())
         }
-        .background(Color(Asset.Colors.backgroundColor.color).ignoresSafeArea())
     }
     
 }
