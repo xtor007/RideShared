47db04d72cfb3f93872d749083e698dc79858ba9
Anatolii Khramchenko <71408259+xtor007@users.noreply.github.com>
2022-12-24 17:36:04 +0200

    Add EnviromentVariables

---
+++ RideShared/Resources/EnviromentVariables.swift
@@ -0,0 +1,12 @@
+//
+//  EnviromentVariables.swift
+//  RideShared
+//
+//  Created by Anatoliy Khramchenko on 24.12.2022.
+//
+
+import Foundation
+
+enum EnviromentVariables {
+    static let googleClientID = "googleClientID"
+}
